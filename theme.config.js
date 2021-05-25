e7e9926d9eabddd6ced72b154987325585496588
Dark Boi <66882633+darkdarcool@users.noreply.github.com>
2021-05-24 18:35:43 -0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,11 +1,11 @@
 export default {
-  repository: 'https://github.com/shuding/nextra',
-  titleSuffix: ' – Nextra',
+  repository: 'https://github.com/darkdarcool/DarkCSS-Docs/',
+  titleSuffix: ' – DarkCSS',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">DarkCSS</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Site Builder
+        Better CSS
       </span>
     </>
   ),
